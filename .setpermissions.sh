308072b19771500a63069477c3aded32a7b93d6a
lojohan <lojohan@student.chalmers.se>
2017-01-22 16:54:06 +0100

    small but ugly improvements to setting perms

--- .setpermissions.sh
+++ .setpermissions.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
-USER=$1
-HOST=$2
-DESTINATION=$3
-DIR=$4
-CHM=$5
-GRP=$6
+if [ "$#" -lt 5 ]
+then
+	echo "Insufficient number of arguments"
+	exit
+fi
 
-ssh "$USER"@"$HOST" "chmod -R "$CHM" "$DESTINATION""$DIR" ; chgrp -R "$GRP" "$DESTINATION""$DIR" && exit"
+if [ "$#" -lt 6 ]
+then
+	USER=$1
+	HOST=$2
+	DESTINATION=$3
+	DIR=$4
+	CHM=$5
+
+	ssh "$USER"@"$HOST" "chmod -R "$CHM" "$DESTINATION""$DIR" && exit"
+fi
+
+if [ "$#" -lt 7 ]
+then
+	USER=$1
+        HOST=$2
+        DESTINATION=$3
+        DIR=$4
+        CHM=$5
+	GRP=$6
+
+	ssh "$USER"@"$HOST" "chmod -R "$CHM" "$DESTINATION""$DIR" ; chgrp -R "$GRP" "$DESTINATION""$DIR" && exit"
+fi
+
+if [ "$#" -ge 7 ]
+then 
+	echo "Too many arguments"
+fi
